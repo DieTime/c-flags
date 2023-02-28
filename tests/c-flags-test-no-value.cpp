631f2a33a511169303c1988dd8f3a3dd1edcb362
Denis Glazkov <glazzk.off@mail.ru>
2023-02-28 23:04:41 +0300

    [c-flags] Add a test in case there is no value after the flag

---
+++ tests/c-flags-test-no-value.cpp
@@ -0,0 +1,62 @@
+#include <c-flags.h>
+#include <gtest/gtest.h>
+
+TEST(CFlagsTestsNoValue, LongNameNoEqual)
+{
+    int *int_value = c_flag_int("test1", "t1", nullptr, 0);
+    const char *argv_raw[] = {"app", "--test1"};
+
+    (void) int_value;
+
+    char **argv = (char **) argv_raw;
+    int argc = (int) (sizeof(argv_raw) / sizeof(argv_raw[0]));
+
+    testing::internal::CaptureStdout();
+
+    EXPECT_EXIT({
+        c_flags_parse(&argc, &argv, false);
+    }, ::testing::ExitedWithCode(1), ".*");
+
+    std::string stdout = testing::internal::GetCapturedStdout();
+    EXPECT_NE(stdout.find("no value for flag"), std::string::npos) << stdout;
+}
+
+TEST(CFlagsTestsNoValue, LongName)
+{
+    int *int_value = c_flag_int("test2", "t2", nullptr, 0);
+    const char *argv_raw[] = {"app", "--test2="};
+
+    (void) int_value;
+
+    char **argv = (char **) argv_raw;
+    int argc = (int) (sizeof(argv_raw) / sizeof(argv_raw[0]));
+
+    testing::internal::CaptureStdout();
+
+    EXPECT_EXIT({
+        c_flags_parse(&argc, &argv, false);
+    }, ::testing::ExitedWithCode(1), ".*");
+
+    std::string stdout = testing::internal::GetCapturedStdout();
+    EXPECT_NE(stdout.find("no value for flag"), std::string::npos) << stdout;
+}
+
+TEST(CFlagsTestsNoValue, ShortName)
+{
+    int *int_value = c_flag_int("test3", "t3", nullptr, 0);
+    const char *argv_raw[] = {"app", "-t3"};
+
+    (void) int_value;
+
+    char **argv = (char **) argv_raw;
+    int argc = (int) (sizeof(argv_raw) / sizeof(argv_raw[0]));
+
+    testing::internal::CaptureStdout();
+
+    EXPECT_EXIT({
+        c_flags_parse(&argc, &argv, false);
+    }, ::testing::ExitedWithCode(1), ".*");
+
+    std::string stdout = testing::internal::GetCapturedStdout();
+    EXPECT_NE(stdout.find("no value for flag"), std::string::npos) << stdout;
+}
